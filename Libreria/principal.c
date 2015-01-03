861310d85fe53affbbf110cadef518b34ed4fa8d
Fiorillo Diego <diegogfiorillo@hotmail.com>
2015-01-02 22:13:43 -0300

    Update principal.c

--- Libreria/principal.c
+++ Libreria/principal.c
@@ -9,6 +9,7 @@
 #include <netdb.h>
 #include <unistd.h>
 
+//Funcion para crear un socket servidor
 int crearSocketServidor(char* puertoCliente)
 {
 
@@ -35,3 +36,61 @@ int crearSocketServidor(char* puertoCliente)
   freeaddrinfo(serverInfo);
   return listenningSocket;
 }
+
+//Funcion para crear un socket cliente
+int crearSocketCliente(char* ipServidor, char* puertoServidor)
+{
+  struct addrinfo hints;
+  struct addrinfo *ServerInfo;
+
+  memset(&hints, 0, sizeof(hints));
+  hints.ai_family = AF_INET;      // Permite que la maquina se encargue de verificar si usamos IPv4 o IPv6
+  hints.ai_socktype = SOCK_STREAM;    // Indica que usaremos el protocolo TCP
+
+  getaddrinfo(ipServidor, puertoServidor, &hints, &ServerInfo);   // Carga en serverInfo los datos de la conexion
+
+  int serverSocket;
+  serverSocket = socket(ServerInfo->ai_family, ServerInfo->ai_socktype, ServerInfo->ai_protocol);
+  if(serverSocket==-1)
+  {
+      printf("Fallo en el socket, se espera 1 minuto y se vuelve a intentar");
+      sleep(6);
+      serverSocket = socket(ServerInfo->ai_family, ServerInfo->ai_socktype, ServerInfo->ai_protocol);
+  }
+  connect(serverSocket, ServerInfo->ai_addr, ServerInfo->ai_addrlen);
+  freeaddrinfo(ServerInfo);   // No lo necesitamos mas
+
+  return serverSocket;
+}
+
+//Crear un archivo de log
+char* archLog = "mi_log";
+logger = log_create("Nombre.c",archLog,1,LOG_LEVEL_INFO);
+
+//Crear semaforo
+sem_t nombre_sem;  //Esto va arriba en la definicion global
+    int semCreado = sem_init(&nombre_sem,1,1);
+    if(semCreado==-1)
+    {
+        log_error(logger,"No pudo crearse el semaforo");
+    }
+
+//Crear estructura del hilo requerido
+    t_pthreadNombre* estructuraHilo = malloc(sizeof(t_pthreadNombre));
+    estructuraHilo->campo1 = elemento1;
+    estructuraHilo->campoN = elementoN;
+
+//Invoco hilo
+pthread_t mythreadNombre;
+        if (pthread_create(&mythreadNombre, NULL, thread_nombre, estructuraHilo)) {
+            log_error(logger,"Error al crear el hilo");
+            abort();
+        }
+ 
+ pthread_join(mythreadNombre,NULL);       //Si es necesario esperar que termine su ejecucion
+ 
+ printf(
+            "    ╔═════════════════════════╗ \n    ║Saludos,Federico y Diego.║ \n    ╚═════════════════════════╝");
+    //Porque era hermoso
+    
+    
